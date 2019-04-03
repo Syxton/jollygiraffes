28a931b8e71e0539637376f4e0209fd0a6f9299b
Matt Davidson <davidso1@rose-hulman.edu>
2019-04-03 14:19:23 -0400

    code cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,78 @@
 $(window).resize(function() {
-  fill_height_width();
-  $(".scroll-pane").sbscroller("refresh");
-  $(".textfill").textfill();
-  smart_scrollbars();
+    fill_height_width();
+    $(".scroll-pane").sbscroller("refresh");
+    $(".textfill").textfill();
+    smart_scrollbars();
 });
 
 var scriptTimer = {
-	scriptRunTime : 0,
-	startTime : 0,
-	stopTime : 0,
-	startTimer : function(){
-		time = new Date();
-		this.startTime = time.getTime();
-	},
-	stopTimer : function(){
-		time = new Date();
-		this.stopTime = time.getTime();
-		this.scriptRunTime = (this.stopTime - this.startTime)/1000;
-	}
+    scriptRunTime: 0,
+    startTime: 0,
+    stopTime: 0,
+    startTimer: function() {
+        time = new Date();
+        this.startTime = time.getTime();
+    },
+    stopTimer: function() {
+        time = new Date();
+        this.stopTime = time.getTime();
+        this.scriptRunTime = (this.stopTime - this.startTime) / 1000;
+    }
 }
 
 function smart_scrollbars() {
-    $('.scroll-content:only-child').css("padding-left","0");
-    $('.slider-wrap').prev('.scroll-content').css("padding-left","46px");
+    $('.scroll-content:only-child').css("padding-left", "0");
+    $('.slider-wrap').prev('.scroll-content').css("padding-left", "46px");
 }
 
-function refresh_all(){
-//scriptTimer.startTimer();
+function refresh_all() {
+    //scriptTimer.startTimer();
     // Make jQuery UI buttons of all buttons and submits and .button that are not alreayd "buttonized"
     $("input:submit, button, .button").not('.ui-button').button();
-//scriptTimer.stopTimer(); alert(scriptTimer.scriptRunTime);
+    //scriptTimer.stopTimer(); alert(scriptTimer.scriptRunTime);
 
-    $(".button").click(function() { return false; });
+    $(".button").click(function() {
+        return false;
+    });
     $(".toggleswitch").toggleSwitch(); // Make toggle switches
     $('.flexsection').click(function() {
-		$(this).next().toggle('blind');
-		return false;
-	}).next().hide();
+        $(this).next().toggle('blind');
+        return false;
+    }).next().hide();
 
     $(".selectable").selectable({
         stop: function() {
-            var func = $( ".ui-selected", this ).attr("rel");
+            var func = $(".ui-selected", this).attr("rel");
             eval(func);
         }
     });
 
-    if($('#admin_display').length){ $('.admin_button').hide(); $('.employee_button').hide(); }else{ $('.admin_button').show();  }
+    if ($('#admin_display').length) {
+        $('.admin_button').hide();
+        $('.employee_button').hide();
+    } else {
+        $('.admin_button').show();
+    }
 
-    if($(".ui-dialog-content").length){
+    if ($(".ui-dialog-content").length) {
         $(".ui-dialog-content").dialog("destroy").remove();
     }
 
-    if($.isFunction($('.nyroModal').nyroModal)) {
+    if ($.isFunction($('.nyroModal').nyroModal)) {
         $('.nyroModal').nyroModal();
     }
 
     $('.keyboard').keyboard({
-        change: function(e, keyboard, el){ var last = $('.keyboard').last(); var cp_value = ucwords(last.val(),true); last.val(cp_value); },
+        change: function(e, keyboard, el) {
+            var last = $('.keyboard').last();
+            var cp_value = ucwords(last.val(), true);
+            last.val(cp_value);
+        },
         stickyShift: false,
         autoAccept: true,
-        position     : {
-            of : $('#display_level'), // optional - null (attach to input/textarea) or a jQuery object (attach elsewhere)
-            at2 : 'center middle'
+        position: {
+            of: $('#display_level'), // optional - null (attach to input/textarea) or a jQuery object (attach elsewhere)
+            at2: 'center middle'
         },
         usePreview: true,
         lockInput: true,
@@ -70,14 +81,14 @@ function refresh_all(){
         }
     });
 
-    setTimeout(function(){
-        fill_height_width();
-        fill_height_width_once();
-        $(".scroll-pane").not(":has(.scroll-content)").sbscroller();
-        $(".scroll-pane").sbscroller("refresh");
-        smart_scrollbars();
-    },
-    100);
+    setTimeout(function() {
+            fill_height_width();
+            fill_height_width_once();
+            $(".scroll-pane").not(":has(.scroll-content)").sbscroller();
+            $(".scroll-pane").sbscroller("refresh");
+            smart_scrollbars();
+        },
+        100);
 
     $(".textfill").textfill();
 
@@ -85,94 +96,100 @@ function refresh_all(){
 
     //Delete old
     $('.colorPicker-picker').remove();
-    $('.colorpicker').colorPicker({ onColorChange : function(id, newValue) { var cp = $('#'+id+'_field'); cp.val(newValue); cp.keyup() }});
-
-    $('.autocapitalizewords').keyup(function(evt){
-      // to capitalize all words
-      var cp_value= ucwords($(this).val(),true) ;
-      $(this).val(cp_value);
+    $('.colorpicker').colorPicker({
+        onColorChange: function(id, newValue) {
+            var cp = $('#' + id + '_field');
+            cp.val(newValue);
+            cp.keyup()
+        }
     });
 
-    $('.autocapitalizefirst').keyup(function(evt){
-      // force: true to lower case all letter except first
-      var cp_value= ucfirst($(this).val(),false) ;
-      $(this).val(cp_value);
+    $('.autocapitalizewords').keyup(function(evt) {
+        // to capitalize all words
+        var cp_value = ucwords($(this).val(), true);
+        $(this).val(cp_value);
+    });
+
+    $('.autocapitalizefirst').keyup(function(evt) {
+        // force: true to lower case all letter except first
+        var cp_value = ucfirst($(this).val(), false);
+        $(this).val(cp_value);
     });
 }
 
-function refresh_tags_editor(){
-    $('.tags_editor').each(function(){
-        if($(this).hasClass('ui-autocomplete-input')){
+function refresh_tags_editor() {
+    $('.tags_editor').each(function() {
+        if ($(this).hasClass('ui-autocomplete-input')) {
 
             var $input = $(this).autocomplete({
-            source: $(this).siblings('.tags_list').val().split(","),
-            minLength: 0
+                source: $(this).siblings('.tags_list').val().split(","),
+                minLength: 0
             });
             $(this).next('.tags_select')
                 .click(function() {
-                        // close if already visible
-                        if ($input.autocomplete("widget").is(":visible")) {
-                            $input.autocomplete("close");
-                            return;
-                        }
-                        $(this).blur();
-                        $input.autocomplete("search", "" );
-                        $input.focus();
-                    });
-        }else{
+                    // close if already visible
+                    if ($input.autocomplete("widget").is(":visible")) {
+                        $input.autocomplete("close");
+                        return;
+                    }
+                    $(this).blur();
+                    $input.autocomplete("search", "");
+                    $input.focus();
+                });
+        } else {
             var $input = $(this).autocomplete({
-            source: $(this).siblings('.tags_list').val().split(","),
-            minLength: 0
+                source: $(this).siblings('.tags_list').val().split(","),
+                minLength: 0
             }).addClass("ui-widget ui-widget-content ui-corner-left");
 
             $("<button type='button'>&nbsp;</button>")
-            .attr("tabIndex", -1)
-            .attr("title", "Show All Items")
-            .insertAfter($input)
-            .button({
-                icons: {
-                    primary: "ui-icon-triangle-1-s"
-                },
-                text: false
-            })
-            .removeClass("ui-corner-all")
-            .addClass("tags_select ui-corner-right ui-button-icon")
-            .click(function() {
-                // close if already visible
-                if ($input.autocomplete("widget").is(":visible")) {
-                    $input.autocomplete( "close" );
-                    return;
-                }
-                $(this).blur();
-                $input.autocomplete("search", "" );
-                $input.focus();
-            });
+                .attr("tabIndex", -1)
+                .attr("title", "Show All Items")
+                .insertAfter($input)
+                .button({
+                    icons: {
+                        primary: "ui-icon-triangle-1-s"
+                    },
+                    text: false
+                })
+                .removeClass("ui-corner-all")
+                .addClass("tags_select ui-corner-right ui-button-icon")
+                .click(function() {
+                    // close if already visible
+                    if ($input.autocomplete("widget").is(":visible")) {
+                        $input.autocomplete("close");
+                        return;
+                    }
+                    $(this).blur();
+                    $input.autocomplete("search", "");
+                    $input.focus();
+                });
         }
     });
 }
 
-function fill_height_width(){
-    $('.fill_height').each(function(){
+function fill_height_width() {
+    $('.fill_height').each(function() {
         var offset = $(this).offset();
         var poffset = $(this).parent().offset();
         var pmargins = $(this).parent().outerHeight(true) - $(this).parent().height();
         var margins = $(this).outerHeight(true) - $(this).height();
         $(this).height((poffset.top + $(this).parent().height() - pmargins) - offset.top - margins);
     });
-    $('.fill_width').each(function(){
+    $('.fill_width').each(function() {
         var offset = $(this).offset();
         var poffset = $(this).parent().offset();
         var margins = $(this).outerWidth(true) - $(this).width();
         $(this).width((poffset.left + $(this).parent().width()) - offset.left);
     });
-    $('.fill_height_middle').each(function(){
+    $('.fill_height_middle').each(function() {
         var offset = $(this).offset();
         var poffset = $(this).parent().offset();
         var pmargins = $(this).parent().outerHeight(true) - $(this).parent().height();
         var margins = $(this).outerHeight(true) - $(this).height();
         $(this).height((poffset.top + $(this).parent().height() - pmargins) - offset.top - margins - $('.bottom').height());
     });
-    $('.fill_width_middle').each(function(){
+    $('.fill_width_middle').each(function() {
         var offset = $(this).offset();
         var poffset = $(this).parent().offset();
         var margins = $(this).outerWidth(true) - $(this).width();
@@ -180,15 +197,15 @@ function fill_height_width(){
     });
 }
 
-function fill_height_width_once(){
-    $('.fill_height_once').each(function(){
+function fill_height_width_once() {
+    $('.fill_height_once').each(function() {
         var offset = $(this).offset();
         var poffset = $(this).parent().offset();
         var pmargins = $(this).parent().outerHeight(true) - $(this).parent().height();
         var margins = $(this).outerHeight(true) - $(this).height();
         $(this).height((poffset.top + $(this).parent().height() - pmargins) - offset.top - margins);
     });
-    $('.fill_width_once').each(function(){
+    $('.fill_width_once').each(function() {
         var offset = $(this).offset();
         var poffset = $(this).parent().offset();
         var margins = $(this).outerWidth(true) - $(this).width();
@@ -196,14 +213,14 @@ function fill_height_width_once(){
     });
 }
 
-function uploader(id,callback,fields){
+function uploader(id, callback, fields) {
     var fd = new FormData();
-    fd.append("afile", $('.uploader'+id+' :file')[0].files[0]);
+    fd.append("afile", $('.uploader' + id + ' :file')[0].files[0]);
     //These extra params aren't necessary but show that you can include other data.
     var i = 0;
-    $.each(fields, function(i){
-        fd.append('values['+i+'][name]', this.name);
-        fd.append('values['+i+'][value]', this.value);
+    $.each(fields, function(i) {
+        fd.append('values[' + i + '][name]', this.name);
+        fd.append('values[' + i + '][value]', this.value);
         i++;
     });
 
@@ -211,19 +228,19 @@ function uploader(id,callback,fields){
     xhr.open('POST', 'ajax/upload.php', true);
 
     xhr.upload.onprogress = function(e) {
-        if (e.lengthComputable && typeof $('.uploader'+id+' :file')[0].files[0] != "undefined" ) {
+        if (e.lengthComputable && typeof $('.uploader' + id + ' :file')[0].files[0] != "undefined") {
             var percentComplete = (e.loaded / e.total) * 100;
-            $('.uploader'+id+' .progress').css("width",percentComplete + '%');
-            if(percentComplete == 100){
-                $('.uploader'+id+' .progress').html('Upload Finished: Processing...');
-            }else{
-                $('.uploader'+id+' .progress').html(percentComplete.toFixed(2) + '%');
+            $('.uploader' + id + ' .progress').css("width", percentComplete + '%');
+            if (percentComplete == 100) {
+                $('.uploader' + id + ' .progress').html('Upload Finished: Processing...');
+            } else {
+                $('.uploader' + id + ' .progress').html(percentComplete.toFixed(2) + '%');
             }
         }
     }
 
     xhr.onload = function() {
-        if (this.status == 200){
+        if (this.status == 200) {
             callback(this.response);
         }
     }
@@ -232,77 +249,95 @@ function uploader(id,callback,fields){
 }
 
 function CreateConfirm(id, messageText, okText, cancelText, okCallback, cancelCallback) {
-    $('#'+id+' label').html(messageText);
-    var thisdialog = $('#'+id);
-    $('#'+id).dialog({
+    $('#' + id + ' label').html(messageText);
+    var thisdialog = $('#' + id);
+    $('#' + id).dialog({
         dialogClass: "big_confirm",
         draggable: false,
         resizable: false,
-        height:300,
+        height: 300,
         modal: true,
         buttons: [{
             text: okText,
-            click : function() {
+            click: function() {
                 $(thisdialog).dialog("destroy");
                 okCallback();
-                }
-            }, {
+            }
+        }, {
             text: cancelText,
             click: function() {
                 $(thisdialog).dialog("destroy");
                 cancelCallback();
-        }}]
+            }
+        }]
     });
 }
 
-function CreateAlert(id,messageText, cancelText, cancelCallback) {
-    $('#'+id+' label').html(messageText);
-    var thisdialog = $('#'+id);
-    $('#'+id).dialog({
+function CreateAlert(id, messageText, cancelText, cancelCallback) {
+    $('#' + id + ' label').html(messageText);
+    var thisdialog = $('#' + id);
+    $('#' + id).dialog({
         dialogClass: "big_confirm",
         draggable: false,
         resizable: false,
-        height:300,
+        height: 300,
         modal: true,
         buttons: [{
             text: cancelText,
             click: function() {
                 $(thisdialog).dialog("destroy");
                 cancelCallback();
-            }}]
+            }
+        }]
     });
 }
 
-function CreateDialog(id,height,width,classes){
-    $('#'+id).dialog({
+function CreateDialog(id, height, width, classes) {
+    $('#' + id).dialog({
         dialogClass: classes,
         draggable: false,
         resizable: false,
         height: height,
         width: width,
         modal: true,
-        open: function(){ this.focus(); },
-        focus: function(){ refresh_tags_editor(); } ,
+        open: function() {
+            this.focus();
+        },
+        focus: function() {
+            refresh_tags_editor();
+        },
         close: function(event, ui) {
-            $('#'+id+' > form').each(function(){ this.reset(); });
+            $('#' + id + ' > form').each(function() {
+                this.reset();
+            });
         }
     });
 }
 
-function numpad(id){
-    var thisdialog = $('#'+id);
-    $('#'+id).dialog({
+function numpad(id) {
+    var thisdialog = $('#' + id);
+    $('#' + id).dialog({
         height: 555,
         width: 290,
         resizable: false,
         draggable: false,
         modal: true,
-        open: function() { $(':input',thisdialog).val(''); var kpad = $('.'+id+'keypad_submit'); kpad.button('destroy'); kpad.children().removeClass("ui-button-text"); kpad.button(); kpad.button('option','disabled',true); }
+        open: function() {
+            $(':input', thisdialog).val('');
+            var kpad = $('.' + id + 'keypad_submit');
+            kpad.button('destroy');
+            kpad.children().removeClass("ui-button-text");
+            kpad.button();
+            kpad.button('option', 'disabled', true);
+        }
     });
 }
 
-function close_modal(){	 $.nmTop().close(); }
-function resize_modal(){
+function close_modal() {
+    $.nmTop().close();
+}
+
+function resize_modal() {
     var contentWidth = $('.printthis').width() + 30;
     var contentHeight = $('.printthis').height() + 30;
 
@@ -313,9 +348,9 @@ function resize_modal(){
     var height = contentHeight > maxHeight ? maxHeight : contentHeight;
 
 
-    $('.printthis .doc').each(function(){ //reduce the size of an image if it is still too big
+    $('.printthis .doc').each(function() { //reduce the size of an image if it is still too big
         var w = $(this).width();
-        $(this).width(w > maxWidth ? maxWidth-100 : w);
+        $(this).width(w > maxWidth ? maxWidth - 100 : w);
     });
 
     $.nmTop().sizes.initW = width;
@@ -324,33 +359,33 @@ function resize_modal(){
     $.nmTop().resize();
 }
 
-function SelectSelectableElements (selectableContainer, elementsToSelect){
+function SelectSelectableElements(selectableContainer, elementsToSelect) {
     $(".ui-selected", selectableContainer).not(elementsToSelect).removeClass("ui-selected").addClass("ui-unselecting");
     $(elementsToSelect).not(".ui-selected").addClass("ui-selecting");
     //selectableContainer.data("selectable")._mouseStop(null);
 }
 
-function ucfirst(str,force){
-  str=force ? str.toLowerCase() : str;
-  return str.replace(/(\b)([a-zA-Z])/,
-           function(firstLetter){
-              return   firstLetter.toUpperCase();
-           });
+function ucfirst(str, force) {
+    str = force ? str.toLowerCase() : str;
+    return str.replace(/(\b)([a-zA-Z])/,
+        function(firstLetter) {
+            return firstLetter.toUpperCase();
+        });
 }
 
-function ucwords(str,force){
-  str=force ? str.toLowerCase() : str;
-  return str.replace(/(\b)([a-zA-Z])/g,
-           function(firstLetter){
-              return   firstLetter.toUpperCase();
-           });
+function ucwords(str, force) {
+    str = force ? str.toLowerCase() : str;
+    return str.replace(/(\b)([a-zA-Z])/g,
+        function(firstLetter) {
+            return firstLetter.toUpperCase();
+        });
 }
 
 refresh_all();
 smart_scrollbars();
 
-$(document).bind("ajaxSend", function(){
+$(document).bind("ajaxSend", function() {
     $(".loadingscreen").show();
-}).bind("ajaxComplete", function(){
+}).bind("ajaxComplete", function() {
     $(".loadingscreen").hide();
 });

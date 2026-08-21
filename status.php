393654c6b1b67b8543a59c4d16498b797ba4d275
Matt Davidson <davidso1@rose-hulman.edu>
2026-08-21 12:04:47 -0400

    bump version

--- status.php
+++ status.php
@@ -31,7 +31,7 @@ $sitename = htmlspecialchars($CFG->sitename);
     <script data-search-pseudo-elements defer
         src="<?php echo $CFG->wwwroot ?>/min/?b=<?php echo $CFG->directory ? $CFG->directory . "/" : ""; ?>scripts/fontawesome&amp;f=fontawesome.min.js,solid.min.js">
     </script>
-    <link rel="stylesheet" href="css/status.css?version=2026081408">
+    <link rel="stylesheet" href="css/status.css?version=2026082101">
     <link rel="shortcut icon" href="favicon.ico" />
 
     <!-- Favicon icons -->
@@ -294,6 +294,6 @@ $sitename = htmlspecialchars($CFG->sitename);
 
 </div>
 
-<script src="scripts/status.js?version=2026081408"></script>
+<script src="scripts/status.js?version=2026082101"></script>
 </body>
 </html>
